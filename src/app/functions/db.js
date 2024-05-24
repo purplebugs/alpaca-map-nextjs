7dbbc4bd353df7759121bd525b672de96df0b5ae
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-24 14:53:51 +0200

    fix: Put back database env logic

--- src/app/functions/db.js
+++ src/app/functions/db.js
@@ -1,6 +1,4 @@
 import Elastic from "@/functions/database.elastic.js";
 import Mock from "@/functions/database.mock.js";
 
-// export const db = process.env.NODE_ENV === "test" ? new Mock() : new Elastic();
-
-export const db = new Elastic();
+export const db = process.env.NODE_ENV === "test" ? new Mock() : new Elastic();
