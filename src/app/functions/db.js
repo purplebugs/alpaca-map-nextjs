281bcee340389e4e892b88567ce957a70f0a9374
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-24 13:43:00 +0200

    fix: Troubleshoot build

--- src/app/functions/db.js
+++ src/app/functions/db.js
@@ -1,4 +1,6 @@
 import Elastic from "@/functions/database.elastic.js";
 import Mock from "@/functions/database.mock.js";
 
-export const db = process.env.NODE_ENV === "test" ? new Mock() : new Elastic();
+// export const db = process.env.NODE_ENV === "test" ? new Mock() : new Elastic();
+
+export const db = new Elastic();
