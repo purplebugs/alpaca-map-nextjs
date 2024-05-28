30a43cb2abf60ada82670c6c1a18a61ced1d3a00
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-28 20:20:01 +0200

    feat: Get data server side example

--- src/app/farm/[id]/page.js
+++ src/app/farm/[id]/page.js
@@ -1,3 +1,13 @@
-export default function Page({ params }) {
-  return <div>My Farm Page: {params.id}</div>;
+import { db } from "@/functions/db.js";
+
+export default async function Page({ params }) {
+  const result = await db.getCompany(params.id);
+
+  console.log("result[0].city", result[0].city);
+
+  return (
+    <div>
+      My Farm Page: {params.id} - City: {result[0].city}
+    </div>
+  );
 }
