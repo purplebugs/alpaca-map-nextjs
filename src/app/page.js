761bc4e97b30b99da60bda10354e5a6529fcb5ae
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-28 14:50:06 +0200

    feat: Skeleton search page, skeleton home page text

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">TODO - Add content here</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">Main Page - Map of alpacas</main>
+  );
 }
---
+++ src/app/search/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+      <header>
+        <h2>Search</h2>
+      </header>
+      <main>TO DO</main>
+    </>
+  );
+}
