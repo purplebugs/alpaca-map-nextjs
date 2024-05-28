4bf44ec3768d49d09ee74f75117602c2b46b508c
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-28 11:25:10 +0200

    test: Body returns at least one result. Refactor to use beforeAll

--- src/app/api/locations/search/route.test.js
+++ src/app/api/locations/search/route.test.js
@@ -1,9 +1,11 @@
-import { describe, test, assert } from "vitest";
+import { describe, beforeAll, test, assert } from "vitest";
 import { POST } from "@/api/locations/search/route.js";
-import { NextRequest } from "next/server";
 
 describe("POST /api/locations/search", () => {
-  test(`POST /api/locations/search { query: "lund" } - HTTP 200`, async () => {
+  let response;
+  let data;
+
+  beforeAll(async () => {
     const formData = new FormData();
     formData.set("query", "lund");
 
@@ -12,23 +14,18 @@ describe("POST /api/locations/search", () => {
       body: formData,
     });
 
-    const response = await POST(request);
+    response = await POST(request);
+    data = await response.json();
+  });
 
+  test(`POST /api/locations/search { query: "lund" } - HTTP 200`, async () => {
     const actual = response.status;
     const expected = 200;
 
     assert.equal(actual, expected, `Response status: ${actual} should be: ${expected}`);
   });
 
-  // TODO use beforeAll then add second test
-  test.skip(`POST /api/locations/search { query: "lund" } should return at least 1 item`, async () => {
-    const formData = new FormData();
-    formData.append("query", "lund");
-
-    const response = await POST(formData);
-
-    const data = await response.json();
-
+  test(`POST /api/locations/search { query: "lund" } should return at least 1 item`, async () => {
     const actual = data.length;
     const expected = 1;
 
