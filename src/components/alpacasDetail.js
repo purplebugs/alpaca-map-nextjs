05045c3099da8a7125dd45f8728d7c6e4dbef1b5
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-29 13:13:15 +0200

    fix: Key to list

--- src/components/alpacasDetail.js
+++ src/components/alpacasDetail.js
@@ -2,5 +2,5 @@ import { AlpacaDetail } from "@/components/alpacaDetail";
 
 // TODO error handling, loading state. eg alpacas.map() without chaining operator "?" throws error
 export const AlpacasDetail = ({ status, alpacas }) => {
-  return alpacas?.map((alpaca) => <AlpacaDetail status={status} alpaca={alpaca} />);
+  return alpacas?.map((alpaca) => <AlpacaDetail key={alpaca.alpacaId} status={status} alpaca={alpaca} />);
 };
