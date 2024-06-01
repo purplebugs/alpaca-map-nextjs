c1a356da7fcc5f7efd791463a5e0851912cddda8
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-01 16:28:15 +0200

    fix: Troubleshoot

--- src/functions/database.elastic.js
+++ src/functions/database.elastic.js
@@ -17,6 +17,8 @@ export default class Database {
         apiKey: process.env.ELASTIC_API_KEY,
       },
     });
+
+    console.log("Testing", process.env.ELASTIC_CLOUD_ID.substring(0, 4));
   }
 
   async getAnimal(id) {
