36b367930d4622c5a47210ebd52cf945ac08e71a
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-01 17:11:43 +0200

    fix: Troubleshoot

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-console.log("Testing config", process.env.ELASTIC_CLOUD_ID.substring(0, 4));
+console.log("Testing config", process?.env?.ELASTIC_CLOUD_ID.substring(0, 4));
 const nextConfig = {};
 
 export default nextConfig;
