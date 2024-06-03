19bf9aba6247298ed5cc30f0320131521924fe8e
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-03 11:58:43 +0200

    fix: Re use db connection if exists

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -4,29 +4,33 @@ import { Client } from "@elastic/elasticsearch";
 import AnimalFetcher from "@/functions/animalFetcher.js";
 import AnimalsFetcher from "@/functions/animalsFetcher.js";
 
+let elastic = null;
+
 const connectToDatabase = () => {
-  const elastic = new Client({
-    cloud: {
-      id: process?.env?.ELASTIC_CLOUD_ID,
-    },
-    auth: {
-      apiKey: process?.env?.ELASTIC_API_KEY,
-    },
-  });
+  if (!elastic) {
+    elastic = new Client({
+      cloud: {
+        id: process?.env?.ELASTIC_CLOUD_ID,
+      },
+      auth: {
+        apiKey: process?.env?.ELASTIC_API_KEY,
+      },
+    });
+  }
 
   return elastic;
 };
 
 export const getAnimal = cache(async (id) => {
-  const elastic = connectToDatabase();
+  const db = connectToDatabase();
 
-  const fetcher = new AnimalFetcher(elastic, id);
+  const fetcher = new AnimalFetcher(db, id);
   return await fetcher.fetch();
 });
 
 export const getAnimals = cache(async (query) => {
-  const elastic = connectToDatabase();
+  const db = connectToDatabase();
 
-  const fetcher = new AnimalsFetcher(elastic, query);
+  const fetcher = new AnimalsFetcher(db, query);
   return await fetcher.fetch();
 });
