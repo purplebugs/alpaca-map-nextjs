e895a21da1a713480752454e48d46985c9b0631e
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-07 10:34:06 +0200

    test: Title and navigation test towards localhost, start server before starting tests

--- test-ui/example.spec.js
+++ test-ui/example.spec.js
@@ -1,18 +1,20 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test.describe("General elements", () => {
+  test("title: has text", async ({ page }) => {
+    await page.goto(`/`);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Alpaca Map/);
+  });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  test("navigation link", async ({ page }) => {
+    await page.goto(`/`);
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+    // Click the link
+    await page.getByRole("link", { name: "Find alpacas on map" }).first().click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+    // Expects the URL to contain string
+    await expect(page).toHaveURL(/\/$/); // URL should end with "/" since it is the home page
+  });
 });
