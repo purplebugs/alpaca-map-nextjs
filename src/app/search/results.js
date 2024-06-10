3b4ede3f278b29418b2b4551f05a1662ac14d196
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-10 18:56:53 +0200

    feat: Show more animal details. Interpret <em> tags

--- src/app/search/results.js
+++ src/app/search/results.js
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import db from "@/functions/db.js";
+
+const RenderedItem = (item) => {
+  // To handle results with highlight markup that represents matches from Elasticsearch
+  const markup = { __html: item };
+
+  // We know this data is clean as it does not come from the the general public
+  return <span dangerouslySetInnerHTML={markup} />;
+};
 
 export default async function Results({ query }) {
   const animals = await db?.getAnimals(query);
 
   console.log(animals);
-  // TODO show more animal details
-  // TODO get farms, areas and show results
-  // TODO interpret emphasis tags in results
-  // TODO debouncing or similar for search
 
+  // TODO get farms, areas and show results
+  // TODO debouncing or similar for search
   // TODO tests
 
   return (
@@ -16,7 +23,20 @@ export default async function Results({ query }) {
       <h4 id="animals-list">🦙 Alpacas - TODO total</h4>
       <ul data-testid="list-results-animals">
         {animals?.map((item) => (
-          <li key={item?.alpacaId}>{item?.alpacaRegisteredName}</li>
+          <li key={item?.alpacaId}>
+            {RenderedItem(item?.alpacaRegisteredName)}
+
+            <ul>
+              <li>Registered Name: {RenderedItem(item?.alpacaRegisteredName)}</li>
+              <li>
+                <Link
+                  href={`/alpaca/${item?.alpacaId}`}
+                  data-testid={`list-results-animals-animal-id-${item?.alpacaId}`}>
+                  Alpaca info
+                </Link>
+              </li>
+            </ul>
+          </li>
         ))}
       </ul>
     </>
