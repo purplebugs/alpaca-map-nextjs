a586e86c1820aba124c86baa3bffba34f852dd9a
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-10 15:01:39 +0200

    feat: Show simple results from search with TODOs

--- src/app/search/results.js
+++ src/app/search/results.js
@@ -1,3 +1,24 @@
+import db from "@/functions/db.js";
+
 export default async function Results({ query }) {
-  return <>TODO results</>;
+  const animals = await db?.getAnimals(query);
+
+  console.log(animals);
+  // TODO show more animal details
+  // TODO get farms, areas and show results
+  // TODO interpret emphasis tags in results
+  // TODO debouncing or similar for search
+
+  // TODO tests
+
+  return (
+    <>
+      <h4 id="animals-list">🦙 Alpacas - TODO total</h4>
+      <ul data-testid="list-results-animals">
+        {animals?.map((item) => (
+          <li key={item?.alpacaId}>{item?.alpacaRegisteredName}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
