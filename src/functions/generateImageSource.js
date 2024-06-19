415a442ba12140297ebfa8d3a0e4b5118e9a6d3b
Anita Lipsky <anita@purplebugs.com>
2024-06-19 15:29:19 +0200

    fix: Use Next.js Image tag for alpaca img

--- src/functions/generateImageSource.js
+++ src/functions/generateImageSource.js
@@ -1,45 +1,49 @@
 export const generateImageSource = (color = null) => {
   const path = "/alpaca_images";
-  const defaultImageSource = "default_alpaca.jpeg";
-  let imageSource = `${path}/${defaultImageSource}`;
+  const defaultSource = "default_alpaca.jpeg";
+  const defaultWidth = 1024;
+  const defaultHeight = 1024;
+  const imageProps = { path: path, source: `${path}/${defaultSource}`, width: defaultWidth, height: defaultHeight };
 
-  if (color === null) {
-    return imageSource;
-  }
-
-  let source = null;
   switch (color) {
-    // TODO continue with all colours
     case "COLOR_WHITE":
-      source = "white_alpaca.jpeg";
+      imageProps.source = `${path}/white_alpaca.jpeg`;
       break;
     case "COLOR_TRUE_BLACK":
-      source = "true_black_alpaca.jpeg";
+      imageProps.source = `${path}/true_black_alpaca.jpeg`;
+      imageProps.width = 812;
+      imageProps.height = 812;
       break;
     case "COLOR_MEDIUM_FAWN":
-      source = "medium_fawn_alpaca.jpeg";
+      imageProps.source = `${path}/medium_fawn_alpaca.jpeg`;
       break;
     case "COLOR_MEDIUM_BROWN":
-      source = "medium_brown_alpaca.jpeg";
+      imageProps.source = `${path}/medium_brown_alpaca.jpeg`;
       break;
     case "COLOR_LIGHT_FAWN":
-      source = "light_fawn_alpaca.jpeg";
+      imageProps.source = `${path}/light_fawn_alpaca.jpeg`;
+      imageProps.width = 1181;
+      imageProps.height = 1181;
       break;
     case "COLOR_BEIGE":
-      source = "beige_alpaca.jpeg";
+      imageProps.source = `${path}/beige_alpaca.jpeg`;
+      imageProps.width = 1181;
+      imageProps.height = 1181;
       break;
     case "COLOR_DARK_BROWN":
-      source = "dark_brown_alpaca.jpeg";
+      imageProps.source = `${path}/dark_brown_alpaca.jpeg`;
       break;
     case "COLOR_LIGHT_BROWN":
-      source = "light_brown_alpaca.jpeg";
+      imageProps.source = `${path}/light_brown_alpaca.jpeg`;
+      imageProps.width = 1181;
+      imageProps.height = 1181;
       break;
     case "COLOR_BAY_BLACK":
-      source = "bay_black_alpaca.jpeg";
+      imageProps.source = `${path}/bay_black_alpaca.jpeg`;
       break;
     default:
-      source = defaultImageSource;
+      return imageProps;
   }
 
-  return `${path}/${source}`;
+  return imageProps;
 };
