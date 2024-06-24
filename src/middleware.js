b4f70a63b2a470a3bdac0e09cdfefd19c1e8fd8a
Anita Lipsky <anita@purplebugs.com>
2024-06-24 11:00:39 +0200

    feat: Middleware sets CORS headers

---
+++ src/middleware.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization", // TODO do not need this
+};
+
+// TODO error handling
+// TODO tests
+
+export function middleware(request) {
+  // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#cors
+
+  // Handle preflighted requests
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  return response;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
