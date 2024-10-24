acfbf27be47bc2db77cb7d0983d117d1cc981af2
Anita Lipsky <7695311+purplebugs@users.noreply.github.com>
2024-10-24 19:23:29 +0200

    style: Search page basic styles.  Re style parents so all pages consistent. (#68)

--- src/app/search/results.js
+++ src/app/search/results.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faMap } from "@fortawesome/free-solid-svg-icons";
+
 import { Pagination } from "@/components/pagination";
 import Link from "next/link";
 import db from "@/functions/db.js";
@@ -7,7 +10,7 @@ const RenderedItem = (item) => {
   const markup = { __html: item };
 
   // We know this data is clean as it does not come from the the general public
-  return <span dangerouslySetInnerHTML={markup} />;
+  return <span className="highlight" dangerouslySetInnerHTML={markup} />;
 };
 
 export default async function Results({
@@ -45,104 +48,205 @@ export default async function Results({
 
   return (
     <>
-      <p data-testid="search-list-results-heading">
-        Areas{" "}
-        {locations?.total > 0 ? (
-          <Link href="#locations-list">{locations?.total}</Link>
-        ) : (
-          0
-        )}{" "}
-        - Farms{" "}
-        {companies?.total > 0 ? (
-          <Link href="#companies-list">{companies?.total}</Link>
-        ) : (
-          0
-        )}{" "}
-        - 🦙 Alpacas{" "}
-        {animals?.total > 0 ? (
-          <Link href="#animals-list">{animals?.total}</Link>
-        ) : (
-          0
-        )}
-      </p>
+      <div
+        data-testid="search-list-results-heading"
+        className="flex flex-wrap justify-center py-2"
+      >
+        <Link
+          href="#locations-list"
+          className="hover:bg-brown-400 hover:underline hover:decoration-8
+          p-1 m-0.5 sm:p-3 sm:m-3 bg-brown-200 rounded-xl shadow-lg 
+          grow shrink-0 basis-20 md:basis-40 
+          flex items-center flex-col justify-between sm:flex-row sm:justify-normal gap-x-4"
+        >
+          <div className="text-center text-2xl">
+            {locations?.total > 0 ? locations?.total : 0}
+          </div>
+          <div className="text-center">
+            <FontAwesomeIcon icon={faMap} className="fa-lg" />
+          </div>
+          <div className="text-center">Areas</div>
+        </Link>
 
-      <h4 id="locations-list">Areas - {locations?.total}</h4>
+        <Link
+          href="#companies-list"
+          className="hover:bg-brown-400 hover:underline hover:decoration-8
+          p-1 m-0.5 sm:p-3 sm:m-3 bg-brown-200 rounded-xl shadow-lg 
+          grow shrink-0 basis-20 md:basis-40 
+          flex items-center flex-col justify-between sm:flex-row sm:justify-normal gap-x-4"
+        >
+          <div className="text-center text-2xl">
+            {companies?.total > 0 ? companies?.total : 0}
+          </div>
+          <div className="text-center">
+            <FontAwesomeIcon icon={faHouse} className="fa-lg" />
+          </div>
+          <div className="text-center">Farms</div>
+        </Link>
 
-      <Pagination
-        items={locationPageList}
-        searchParams={searchParams}
-        section={"locationPageNumber"}
-      />
+        <Link
+          href="#animals-list"
+          className="hover:bg-brown-400 hover:underline hover:decoration-8
+          p-1 m-0.5 sm:p-3 sm:m-3 bg-brown-200 rounded-xl shadow-lg 
+          grow shrink-0 basis-20 md:basis-40 
+          flex items-center flex-col justify-between sm:flex-row sm:justify-normal gap-x-4"
+        >
+          <div className="text-center text-2xl">
+            {animals?.total > 0 ? animals?.total : 0}
+          </div>
+          <div className="text-center text-2xl">🦙</div>
+          <div className="text-center">Alpacas</div>
+        </Link>
+      </div>
 
-      <ul data-testid="list-results-locations">
-        {locations?.items?.map((item) => (
-          <li key={item?.id}>
-            <Link
-              href={`/farm/${item?.id}`}
-              data-testid={`list-results-locations-farm-id-${item?.id}`}
-            >
-              {RenderedItem(item?.name)}
-            </Link>{" "}
-            -{" "}
-            {RenderedItem(item?.location?.google?.administrative_area_level_2)},{" "}
-            {RenderedItem(item?.location?.google?.administrative_area_level_1)}
-          </li>
-        ))}
-      </ul>
+      {locations?.total > 0 && (
+        <div id="locations-list" className="my-6 space-y-4">
+          <h3 className="text-2xl font-bold leading-loose tracking-tight">
+            <FontAwesomeIcon icon={faMap} className="fa-lg" /> Areas -{" "}
+            {locations?.total}
+          </h3>
 
-      <h4 id="companies-list">Farms - {companies?.total}</h4>
+          <ul data-testid="list-results-locations" className="space-y-4">
+            {locations?.items?.map((item) => (
+              <li key={item?.id} className="flex items-center">
+                <svg
+                  className="h-6 w-6 flex-none fill-brown-100 stroke-brown-500 stroke-2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                >
+                  <circle cx="12" cy="12" r="11" />
+                  <path
+                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                    fill="none"
+                  />
+                </svg>
 
-      <Pagination
-        items={farmPageList}
-        searchParams={searchParams}
-        section={"farmPageNumber"}
-      />
+                <div className="ml-4">
+                  <Link
+                    href={`/farm/${item?.id}`}
+                    data-testid={`list-results-locations-farm-id-${item?.id}`}
+                    className="hover:underline hover:decoration-8"
+                  >
+                    {RenderedItem(item?.name)}
+                  </Link>{" "}
+                  -{" "}
+                  {RenderedItem(
+                    item?.location?.google?.administrative_area_level_2
+                  )}
+                  ,{" "}
+                  {RenderedItem(
+                    item?.location?.google?.administrative_area_level_1
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
 
-      <ul data-testid="list-results-companies">
-        {companies?.items?.map((item) => (
-          <li key={item?.id}>
-            <Link
-              href={`/farm/${item.id}`}
-              data-testid={`list-results-companies-farm-id-${item.id}`}
-            >
-              {RenderedItem(item.name)}
-            </Link>
-          </li>
-        ))}
-      </ul>
+          <Pagination
+            items={locationPageList}
+            searchParams={searchParams}
+            section={"locationPageNumber"}
+          />
+        </div>
+      )}
 
-      <h4 id="animals-list">🦙 Alpacas - {animals?.total}</h4>
+      {companies?.total > 0 && (
+        <div id="companies-list" className="my-6 space-y-4">
+          <h3 className="text-2xl font-bold leading-loose tracking-tight">
+            <FontAwesomeIcon icon={faHouse} className="fa-lg" /> Farms -{" "}
+            {companies?.total}
+          </h3>
 
-      <Pagination
-        items={alpacaPageList}
-        searchParams={searchParams}
-        section={"alpacaPageNumber"}
-      />
+          <ul data-testid="list-results-companies" className="space-y-4">
+            {companies?.items?.map((item) => (
+              <li key={item?.id} className="flex items-center">
+                <svg
+                  className="h-6 w-6 flex-none fill-brown-100 stroke-brown-500 stroke-2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                >
+                  <circle cx="12" cy="12" r="11" />
+                  <path
+                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                    fill="none"
+                  />
+                </svg>
 
-      <ul data-testid="list-results-animals">
-        {animals?.items?.map((item) => (
-          <li key={item?.alpacaId}>
-            <p>
-              Short Name:{" "}
-              <Link
-                href={`/alpaca/${item?.alpacaId}`}
-                data-testid={`list-results-animals-animal-id-${item?.alpacaId}-short-name`}
-              >
-                {RenderedItem(item?.alpacaRegisteredName)}
-              </Link>
-            </p>
-            <p>
-              Registered Name:{" "}
-              <Link
-                href={`/alpaca/${item?.alpacaId}`}
-                data-testid={`list-results-animals-animal-id-${item?.alpacaId}-registered-name`}
-              >
-                {RenderedItem(item?.alpacaRegisteredName)}
-              </Link>
-            </p>
-          </li>
-        ))}
-      </ul>
+                <div className="ml-4">
+                  <Link
+                    href={`/farm/${item.id}`}
+                    data-testid={`list-results-companies-farm-id-${item.id}`}
+                    className="hover:underline hover:decoration-8"
+                  >
+                    {RenderedItem(item.name)}
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <Pagination
+            items={farmPageList}
+            searchParams={searchParams}
+            section={"farmPageNumber"}
+          />
+        </div>
+      )}
+
+      {animals?.total > 0 && (
+        <div id="animals-list" className="my-6 space-y-4">
+          <h3 className="text-2xl font-bold leading-loose tracking-tight">
+            🦙 Alpacas - {animals?.total}
+          </h3>
+
+          <ul data-testid="list-results-animals" className="space-y-4">
+            {animals?.items?.map((item) => (
+              <li key={item?.alpacaId} className="flex items-center">
+                <svg
+                  className="h-6 w-6 flex-none fill-brown-100 stroke-brown-500 stroke-2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                >
+                  <circle cx="12" cy="12" r="11" />
+                  <path
+                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                    fill="none"
+                  />
+                </svg>
+
+                <div className="ml-4">
+                  Short Name:{" "}
+                  <Link
+                    href={`/alpaca/${item?.alpacaId}`}
+                    data-testid={`list-results-animals-animal-id-${item?.alpacaId}-short-name`}
+                    className="hover:underline hover:decoration-8"
+                  >
+                    {RenderedItem(item?.alpacaShortName)}
+                  </Link>
+                  <ul>
+                    <li>
+                      Registered Name:{" "}
+                      <Link
+                        href={`/alpaca/${item?.alpacaId}`}
+                        data-testid={`list-results-animals-animal-id-${item?.alpacaId}-registered-name`}
+                        className="hover:underline hover:decoration-8"
+                      >
+                        {RenderedItem(item?.alpacaRegisteredName)}
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <Pagination
+            items={alpacaPageList}
+            searchParams={searchParams}
+            section={"alpacaPageNumber"}
+          />
+        </div>
+      )}
     </>
   );
 }
