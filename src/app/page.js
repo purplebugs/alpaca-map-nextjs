c63c6422f94577fbfe70bface42395640143ab33
Trygve Lie <173003+trygve-lie@users.noreply.github.com>
2024-10-28 23:17:34 +1000

    chore: Use Web Component Map (#66)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+// The two following lines are due to this issue in next.js and web components: https://github.com/vercel/next.js/issues/70502 
+// This is a workaround: https://github.com/lit/lit/issues/4771
+// When React 19, or with a later version of Lit, this workaround is probably not needed and this can be removed.
+import {HTMLElement} from '@lit-labs/ssr-dom-shim';
+globalThis.HTMLElement ??= HTMLElement;
+
+import { AlpacaMap } from "@/components/map.js";
+
 export default function Home() {
   return (
     <main>
-      Main Page - Map of alpacas
-      <h1 className="text-3xl font-bold underline">
-        Tailwind styling in Next.js!
-      </h1>
+      <AlpacaMap apikey="AIzaSyDRbvgttCBwylkqM9mcdBM5FoFEUKinng0"></AlpacaMap>
     </main>
   );
 }
---
+++ src/components/map.js
@@ -0,0 +1,11 @@
+"use client";
+
+import * as React from "react";
+import { createComponent } from "@lit/react";
+import alpacaMap from "@purplebugs/alpaca-map";
+
+export const AlpacaMap = createComponent({
+  tagName: "alpaca-map",
+  elementClass: alpacaMap,
+  react: React,
+});
