eceb57fc977379185351f418eba02517864de391
Trygve Lie <trygve.lie@finn.no>
2024-10-28 14:10:51 +0100

    chore: Comment workaround for HTMLElement error

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
-// The two following lines are due here due to: https://github.com/lit/lit/issues/4771
+// The two following lines are due to this issue in next.js and web components: https://github.com/vercel/next.js/issues/70502 
+// This is a workaround: https://github.com/lit/lit/issues/4771
+// When React 19, or with a later version of Lit, this workaround is probably not needed and this can be removed.
 import {HTMLElement} from '@lit-labs/ssr-dom-shim';
 globalThis.HTMLElement ??= HTMLElement;
 
