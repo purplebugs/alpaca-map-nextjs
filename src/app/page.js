93d2992d00e5c010efee12381e816aa4099cce8e
Trygve Lie <trygve.lie@finn.no>
2024-10-28 14:00:05 +0100

    test: Fix tests

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+// The two following lines are due here due to: https://github.com/lit/lit/issues/4771
+import {HTMLElement} from '@lit-labs/ssr-dom-shim';
+globalThis.HTMLElement ??= HTMLElement;
+
 import { AlpacaMap } from "@/components/map.js";
 
 export default function Home() {
